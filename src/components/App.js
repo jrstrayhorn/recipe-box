fefea771b393290b041837a4a8250f42495829f9
stepforwardtech <jrstrayhorn@gmail.com>
2017-09-11 06:40:41 -0400

    Getting AddNewRecipe container working, using react-redux connect function to wire up action to form

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import { Component } from 'react'
 import Header from './ui/Header'
 import Footer from './ui/Footer'
-import { Recipes } from './containers'
+import { Recipes, NewRecipe } from './containers'
 
 class App extends Component {
     render() {
         return (
             <div className="container">
                 <Header title="Recipe Box" />
+                <NewRecipe />
                 <Recipes />
                 <Footer />
             </div>
--- src/components/containers.js
+++ src/components/containers.js
@@ -1,5 +1,16 @@
 import { connect } from 'react-redux'
 import RecipeList from './ui/RecipeList'
+import AddRecipeForm from './ui/AddRecipeForm'
+import { addRecipe } from '../actions'
+
+export const NewRecipe = connect(
+    null,
+    dispatch => ({
+        onNewRecipe(name, ingredients) {
+            dispatch(addRecipe(name, ingredients))
+        }
+    })
+)(AddRecipeForm)
 
 export const Recipes = connect(
     state => {
